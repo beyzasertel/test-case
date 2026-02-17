81ae93240278e85461e7940facd76f24898f5ebb
beyzasertel <beyza786@outlook.com>
2026-02-17 10:06:29 +0300

    initial commit

---
+++ pages/app.jsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.scss";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2563eb",
+    },
+    secondary: {
+      main: "#64748b",
+    },
+  },
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
